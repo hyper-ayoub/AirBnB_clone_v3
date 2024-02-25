132839708554bf8e6d8514ae59dc183bcc3c91d8
hyperayoub <bouagnaayoub@gmail.com>
2024-02-25 16:19:24 +0000

    models

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,6 +1,4 @@
-#!/usr/bin/python3
-"""
-Contains the class DBStorage
+ontains the class DBStorage
 """
 
 import models
@@ -76,17 +74,22 @@ class DBStorage:
         self.__session.remove()
 
     def get(self, cls, id):
-        """query on the current database session"""
-        if cls:
-            obj = self.__session.query(cls).get(id)
-            return obj
+        '''get:
+        retrieve an object from the file storage by class and id.
+        '''
+        if cls in classes.values() and id and type(id) == str:
+            d_obj = self.all(cls)
+            for key, value in d_obj.items():
+                if key.split(".")[1] == id:
+                    return value
         return None
 
     def count(self, cls=None):
-        """Returns the number of objects in storage matching the given class"""
-        if cls:
-            all_objs_dict = self.all(cls)
-            count = len(all_objs_dict)
-        else:
-            count = len(self.all())
-        return count
+        '''count:
+        count the number of objects in storage matching the given class.
+        '''
+        data = self.all(cls)
+        if cls in classes.values():
+            data = self.all(cls)
+        return len(data)
+
