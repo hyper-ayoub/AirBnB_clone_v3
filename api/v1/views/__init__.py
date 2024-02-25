fb465ec5c422530a2a34329fd468fbc0ea526d6b
ayoub bouagna <133155846+hyper-ayoub@users.noreply.github.com>
2024-02-25 20:15:02 +0100

    Update __init__.py

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
-"""sharing app_views Blueprint"""
+"""
+Creates a Blueprint instance with `url_prefix` set to `/api/v1`.
+"""
 
 from flask import Blueprint
-app_views = Blueprint('app_views', __name__)
+
+app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
+
 from api.v1.views.index import *
 from api.v1.views.states import *
 from api.v1.views.cities import *
