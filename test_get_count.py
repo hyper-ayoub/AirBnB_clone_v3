dd8cb2735aeed8b3d48db856afc4e600002c5272
ayoub bouagna <133155846+hyper-ayoub@users.noreply.github.com>
2024-02-26 22:40:59 +0100

    Update test_get_count.py

--- test_get_count.py
+++ test_get_count.py
@@ -4,19 +4,8 @@
 from models import storage
 from models.state import State
 
-# Count all objects
 print("All objects: {}".format(storage.count()))
-
-# Count State objects
 print("State objects: {}".format(storage.count(State)))
 
-# Get all State objects
-all_states = list(storage.all(State).values())
-
-if all_states:
-    # If there are State objects, get the first state's ID
-    first_state_id = all_states[0].id
-    print("First state: {}".format(storage.get(State, first_state_id)))
-else:
-    print("No State objects found.")
-
+first_state_id = list(storage.all(State).values())[0].id
+print("First state: {}".format(storage.get(State, first_state_id)))
