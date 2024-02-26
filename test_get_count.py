486c426b5dfcb3791ec04dcc56c0ae4772aa1c6e
hyperayoub <bouagnaayoub@gmail.com>
2024-02-26 14:36:41 -0800

    update

--- test_get_count.py
+++ test_get_count.py
@@ -4,8 +4,16 @@
 from models import storage
 from models.state import State
 
-print("All objects: {}".format(storage.count()))
-print("State objects: {}".format(storage.count(State)))
+# Count all objects
+all_objects_count = storage.count()
 
-first_state_id = list(storage.all(State).values())[0].id
-print("First state: {}".format(storage.get(State, first_state_id)))
+# Count State objects
+state_objects_count = storage.count(State)
+
+# Get the first state
+first_state = list(storage.all(State).values())[0]
+
+print("All objects: {}".format(all_objects_count))
+print("State objects: {}".format(state_objects_count))
+print("First state: {}".format(first_state))
+
